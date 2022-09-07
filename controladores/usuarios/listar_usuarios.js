a5811cbbf05fa4a3a6e789d3630956b2ab455371
Giuliana Zambotto Furlan <giuliana.furlan@ambevtech.com.br>
2022-09-07 15:56:00 -0300

    ultima 8

--- controladores/usuarios/listar_usuarios.js
+++
@@ -1,9 +0,0 @@
-const conexao = require('../../conexao');
-
-const getUsers = async (req, res) => {
-  const { rows } = await conexao.query('SELECT * FROM base_users');
-  rows.shift();
-  return res.status(400).json(rows);
-}
-
-module.exports = getUsers;
--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./servidor');
-
-app.listen(5000, () => {
-  console.log(`App listening on port ${port}`)
-})
-
-module.exports = app;
